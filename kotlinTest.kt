bcac3e902a659f28aa94ffb5ffa313dc58615492
YoungCheol-Lee <154402400+YoungCheol-Lee@users.noreply.github.com>
2025-01-31 17:24:29 +0900

    Add counter

--- kotlinTest.kt
+++ kotlinTest.kt
@@ -1,26 +1,40 @@
 // Kotlin 테스트 
+
+// 0. class란?
+// 클래스, 객체, 생성자(속성=property=constructor로 만든 변수들)
+
 // 1. 사칙연산 계산기 클래스
 // 두 개의 값을 받아 사칙연산 하는 코드 
 // 결과 : "답은 00 입니다." 출력 혹은 return
 // 0 나눗셈 시, "계산할 수 없습니다." return
 
-class Calculating constructor(first_num:Int, cal_sym:String, sec_num:Int){
-    if(cal_sym = "+"){
-        print(first_num + sec_num)
-    }
-    else if(cal_sym="-"){
-        print(first_num - sec_num)
-    }
-    else if(cal_sym="*"){
-        print(first_num * sec_num)
-    }
-    else if(cal_sym="/"){
-        if(first_num=0){
-            print("계산할 수 없습니다.")
+class Calculater {
+    var first_num:Int
+    var cal_sym:String
+    var sec_num:Int
+    // function으로 Calculating.cal_sym = "+" 일때,
+    // ${Calculating.first_num + Calculating.sec_num}
+    // class로 객체 생성 : val cal1 = Calculating(3, "+", 1)
+    // 함수명(cal1) : class에 있는 함수명으로 해당 객체(cal1)에 할당된 값으로 함수 실행
+    fun Calculating(){
+        if(Calculater.cal_sym="+"){
+            println("답은 ${Calculater.first_num + Calculater.sec_num}")
         }
-        print(first_num/sec_num)
+        else if(Calculater.cal_sym="-"){
+            println("답은 ${Calculater.first_num + Calculater.sec_num}")
+        }
+        else if(Calculater.cal_sym="*"){
+           println("답은 ${Calculater.first_num * Calculater.sec_num}")
+        }
+        else if(Calculater.cal_sym="%"){
+            if(Calculater.first_num = 0){
+                println("계산할 수 없습니다.")
+            }
+            else(){
+                println("답은 ${Calculater.first_num % Calculater.sec_num}")
+            }
+        }
     }
-}
 
 
 // 2. 자동차 클래스
